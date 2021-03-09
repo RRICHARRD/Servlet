03631426196d4f3e8bf20eea7bbb2a2bcd7f13fc
RRICHARRD <guilherme.rixard@gmail.com>
2021-03-09 12:19:44 -0300

    Servlet mapping Configured at DD file on Web.xml inside directory WEB-INF. This is the other way to use a servlet, is without Annotation (@WebServlet())

--- src/com/application/controller/SignServletController.java
+++ src/com/application/controller/SignServletController.java
@@ -4,32 +4,29 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "ServletController", urlPatterns = "/servletController")
-public class ServletController extends HttpServlet {
+public class SignServletController extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		PrintWriter out = response.getWriter();
+		final PrintWriter out = response.getWriter();
+	
 		out.println("<html>");
 		out.println("<head>");
-		out.println("<title>[SERVLET] - indexView.html</title>");
+		out.println("<title> </title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1>html page with link to servlet</h1>");
-		out.println("<a href=\"/servlets/indexView.html\">jump to indexView.html</a>");
+		out.println("<h1> Sign Servelet </h2>");
+		out.println("<a href=\"/servlets/indexView.html\">Jump to indexView.html </a>");
 		out.println("</body>");
 		out.println("</html>");
-		out.println("Servlet functioning");
 		out.close();
 	}
-	
 }
